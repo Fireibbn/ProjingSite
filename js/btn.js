ff1f8201dcb87a9141399b0c41f7032939bf9db5
Fireibbn <92108724+Fireibbn@users.noreply.github.com>
2022-01-15 19:15:46 -0300

    Add files via upload

---
+++ js/btn.js
@@ -0,0 +1,25 @@
+setTimeout (function nowCheck() {
+    // Get all the child elements inside the DIV.
+    var cont = document.getElementById('maps2').addEventListener("click");  
+    
+            if (cont.checked) {
+              window.open('https://goo.gl/maps/zU2oVW2cPkg4tjKRA');
+                cont.checked =false;
+                nowCheck="";
+            }
+
+    
+},4000);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/cardMaterial.js
@@ -0,0 +1,14 @@
+$('#circleDrop1').click(function(){
+  $('.card-middle1').slideToggle();
+  $('.close1').toggleClass('closeRotate1');
+});
+
+$('#circleDrop2').click(function(){
+  $('.card-middle2').slideToggle();
+  $('.close2').toggleClass('closeRotate2');
+});
+
+$('#circleDrop3').click(function(){
+  $('.card-middle3').slideToggle();
+  $('.close3').toggleClass('closeRotate3');
+});
